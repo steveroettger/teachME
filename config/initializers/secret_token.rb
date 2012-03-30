50c5543ecd90c235ff40dcb2d774f1eaec39a8e3
Steve Roettger <steve@steveroettger.com>
2012-03-29 21:24:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachME::Application.config.secret_token = '44db386d3a6a933440ab3ce5e41e7cd57e65425b7f09ca5d04b66b5254b2e465d4aafdd99ff5affc5ec6f88e81451a39c7e1175045b03629613c5aebffec8273'
