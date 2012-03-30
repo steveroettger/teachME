8751d322d064fd6e195ae5d81898aca6fbb93f33
Steve Roettger <steve@steveroettger.com>
2012-03-29 21:36:37 -0400

    Added a users scaffold & pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+	def home
+	  @title = "Home"
+	  render 'home'
+	end
+	
+	def about
+	  @title = "About"
+	  render 'about'
+	end
+	
+	def contact
+	  @title = "Contact"
+	  render 'contact'
+	end 
+
+end
